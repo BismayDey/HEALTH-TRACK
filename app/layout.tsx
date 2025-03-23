be9645fe1c87e0d0d6cb494b46934308bbcb5393
BismayDey <bismaydey001@gmail.com>
2025-03-23 15:52:35 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/bh.pngs" type="image/x-icon" />
+        <link rel="icon" href="/bh.png" type="image/x-icon" />
       </head>
       <body>{children}</body>
     </html>
